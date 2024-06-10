9f77c1785900520f6b4b5eba8331249e535cdbe3
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-11 10:45:53 +1200

    Extension task
    Added age question to questionnaire.

--- code/1-welcome.js
+++ code/1-welcome.js
@@ -19,9 +19,14 @@ function questionnaire() {
   let name = prompt ('What is your name')
   alert ('Hello ' + name + '!')
 
-  let breakfast = prompt ('What did you have for breakfast this morning')
+  let breakfast = prompt ('What did you have for breakfast this morning ' + name + '?')
   alert ('Yum, I love having ' + breakfast + ' for breakfast')
 
-  let colour = prompt ('Finally, what is your favourite colour?')
+  let colour = prompt ('What is your favourite colour?')
   alert ('Oooh I like the colour ' + colour + '!')
+
+  let age = prompt ('Finally, how old are you?')
+  age = Number (age)
+  age=age+1
+  alert ('That must mean on your next birthday you will be turning ' + age + '!')
 }
