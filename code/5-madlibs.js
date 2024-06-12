f9942fdd5a5571ba6c83e6f3fc2da2375ccc76c5
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-13 10:14:13 +1200

    madlibs done

--- code/5-madlibs.js
+++ code/5-madlibs.js
@@ -3,5 +3,10 @@
 // ####################################
 
 function madLibs() {
-  
+  let exclamation = prompt ('Give me an exclamation...')
+  let adverb = prompt ('Give me an adverb...')
+  let noun = prompt ('Give me a noun...')
+  let adjective = prompt ('Give me an adjective...')
+
+  alert (exclamation + '! he said ' + adverb + ' as he jumped into his brand new ' + noun + ' and drove of with his ' + adjective + ' wife.' )
 }
