1ff1583f437c4c7ff30ec518e3b4a38ca8c178a0
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-06-12 15:13:45 +1200

    Mihi's

--- code/3-non-maori-mihi.js
+++ code/3-non-maori-mihi.js
@@ -8,12 +8,18 @@ function nonMaoriMihi() {
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
   alert('What is your name?')
-  let name = prompt()
-  // more questions and answers here
+  let name = prompt('What is your name?')
+  let mountain = prompt ('What mountain shelters you?')
+  let river = prompt ('What is your favourite river?')
+  let town = prompt ('Where would you consider your home town?')
 
   alert('Your mihi:')
-  // more outputs here
-
+  alert ('Tēnā koutou,')
+  alert ('Kei te mihi au ki a ' + mountain + ',')
+  alert ('Kei te mihi au ki a ' + river + ',')
+  alert ('Ko Te Tiriti o Waitanga te waka,')
+  alert ('Ko Aotearoa te iwi')
+  alert ('Nō ' + town + ' au,')
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
